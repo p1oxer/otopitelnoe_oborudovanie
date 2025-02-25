76ac976b08f2d07203d8d6b35c38fb3bcd2ada61
p1oxer <pabonna@mail.ru>
2025-02-25 15:25:32 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.querySelector(".icon-menu").addEventListener("click", () => {
+    document.body.classList.toggle("lock");
+    document.body.classList.toggle("menu-open");
+});
+
+document.querySelectorAll('a[href^="#"').forEach((link) => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        let href = this.getAttribute("href").substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = 30; 
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: "smooth",
+        });
+    });
+});
