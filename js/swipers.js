0e04abedfb36a6449ae2b8f98a5f06eef7ad0f33
p1oxer <pabonna@mail.ru>
2025-02-28 13:12:33 +0300

    about DONE

--- js/swipers.js
+++ js/swipers.js
@@ -5,11 +5,22 @@ const introSwiper = new Swiper(".body-intro__slider", {
         nextEl: ".body-intro__swiper-btn-next",
     },
     pagination: {
-        el: ".swiper-pagination-custom",
+        el: ".intro-pagination",
         clickable: true,
     },
     autoplay: {
         delay: 3000,
     },
     loop: true,
+});
+const aboutSwiper = new Swiper(".body-about__slider", {
+    spaceBetween: 10,
+    navigation: {
+        prevEl: ".body-about__swiper-btn-prev",
+        nextEl: ".body-about__swiper-btn-next",
+    },
+    pagination: {
+        el: ".about-pagination",
+        clickable: true,
+    },
 });
