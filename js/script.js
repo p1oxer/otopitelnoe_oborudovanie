a7e731c40528de221333689144ecdec590c1d1bb
p1oxer <pabonna@mail.ru>
2025-03-09 15:34:49 +0300

    certificates DONE

--- js/script.js
+++ js/script.js
@@ -58,3 +58,25 @@ window.addEventListener("resize", equalizeHeights);
 //         });
 //     });
 // });
+
+const tabButtons = document.querySelectorAll(".body-sertificates__buttons-item");
+const tabContents = document.querySelectorAll(".body-sertificates__item");
+
+if (tabButtons && tabContents) {
+    tabButtons.forEach((tabBtn) => {
+        tabBtn.addEventListener("click", () => {
+            const tabId = tabBtn.getAttribute("data-id");
+
+            tabButtons.forEach((btn) => btn.classList.remove("active"));
+            tabBtn.classList.add("active");
+
+            tabContents.forEach((content) => {
+                content.classList.remove("active");
+
+                if (content.id === tabId) {
+                    content.classList.add("active");
+                }
+            });
+        });
+    });
+}
